ba02e13b8782e80e48fe3ca185e2d2d6482d4c28
Fabian Cardaci <fcardaci@rebanking.com>
2019-05-11 22:51:01 -0300

    Project creation

---
+++ src/main/java/com/utn/ia/recital/RecitalApplication.java
@@ -0,0 +1,13 @@
+package com.utn.ia.recital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecitalApplication.class, args);
+    }
+
+}
